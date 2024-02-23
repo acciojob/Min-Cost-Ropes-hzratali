ac47f8980d5a3e4d711eedffbc6e4e95c2669278
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-02-24 00:06:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr
+	const PriorityQueue = require('priorityqueuejs');
+    const pq = new PriorityQueue((a, b) => a - b
+    for (const rope of arr) {
+        pq.enq(rope);
+    }
+    let totalCost = 0;
+    while (pq.size() > 1) {
+        const shortest1 = pq.deq();
+        const shortest2 = pq.deq();
+        const cost = shortest1 + shortest2;
+        totalCost += cost;
+        pq.enq(cost);
+    }
+    return totalCost;
 }
 
 module.exports=mincost;
